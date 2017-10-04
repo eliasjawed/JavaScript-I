eeea45d9607a584ebe3361c60c24d1c95d8bc72e
eliasjawed <contact@elijawed.com>
2017-10-04 13:30:43 -0700

    Homework

--- src/objects.js
+++ src/objects.js
@@ -5,22 +5,44 @@ const keys = (obj) => {
   // Retrieve all the names of the object's properties.
   // Return the keys as strings in an array.
   // Based on http://underscorejs.org/#keys
+  const names = [];
+
+  Object.keys(obj).forEach((key) => {
+    names.push(key);
+  });
+  return names;
 };
 
 const values = (obj) => {
   // Return all of the values of the object's own properties.
   // Ignore functions
   // http://underscorejs.org/#values
+  const newValues = [];
+
+  Object.keys(obj).forEach((key) => {
+    newValues.push(obj[key]);
+  });
+  return newValues;
 };
 
 const mapObject = (obj, cb) => {
   // Like map for arrays, but for objects. Transform the value of each property in turn.
   // http://underscorejs.org/#mapObject
+  const transValue = {};
+  Object.keys(obj).forEach((key) => {
+    transValue[key] = cb(obj[key]);
+  });
+  return transValue;
 };
 
 const pairs = (obj) => {
   // Convert an object into a list of [key, value] pairs.
   // http://underscorejs.org/#pairs
+  const pairsArray = [];
+  Object.keys(obj).forEach((key) => {
+    pairsArray.push([key, obj[key]]);
+  });
+  return pairsArray;
 };
 
 /* Extra credit */
